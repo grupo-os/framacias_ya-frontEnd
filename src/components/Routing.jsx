b152c888c1fba7526f6643efc427dd723c203f15
santiR22 <santiagorodas688@gmail.com>
2021-12-09 14:51:54 -0300

    .

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -5,16 +5,30 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Farmacia } from "../pages/Farmacia";
 import { FormularioProductos } from "../pages/FormularioProductos";
+import { useState, useEffect } from "react";
+import JsonData from "../data/data.json";
 
 export const Routing = () => {
-    return (
-      <Routes>
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/post" element={<Post />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/farmacia" element={<Farmacia />} />
-        <Route exact path="/formulario-productos" element={<FormularioProductos />} />
-      </Routes>
-    );
-  };
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/home"
+        element={<Home data={landingPageData.Header} />}
+      />
+      <Route exact path="/post" element={<Post />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+      <Route exact path="/farmacia" element={<Farmacia />} />
+      <Route
+        exact
+        path="/formulario-productos"
+        element={<FormularioProductos />}
+      />
+    </Routes>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import "../styles/Home.css"
+import "../styles/Home.css";
 
-export const Home = () => {
+export const Home = (props) => {
   return (
-    <div className="container ">
-      <div className="abs-center">
-        <h1>BIENVENIDO</h1>
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#features"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
