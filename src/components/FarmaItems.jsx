750eca42b5c43fc8a1d6632dc570e8625a5be078
juanosorioh <juangabrielosorioheredia@gmail.com>
2021-12-12 17:43:29 -0300

    first commit

--- src/components/FarmaItems.jsx
+++ src/components/FarmaItems.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import "../styles/Spin.css";
 import "animate.css";
+import { useNavigate } from "react-router";
 
 export const FarmaItems = () => {
+  navigate = useNavigate();
   const value = useContext(DataContext);
   const addCarrito = value.addCarrito;
   const fetchMedicamentos = value.fetchMedicamentos;
@@ -16,30 +18,47 @@ export const FarmaItems = () => {
   }, []);
   console.log(value.items._id);
   return items.length > 0 ? (
+
     items.map((item) => {
       return (
-        <div className="producto animate__animated animate__fadeInUp">
-          <div className="producto__img">
-            <img src={item.img} alt="img" />
-          </div>
+        
+        item.activo ? (
+          <div className="producto">
+          <Link to="#">
+            <div className="producto__img">
+              <img src={item.img} alt="img" />
+            </div>
+          </Link>
           <div className="producto__footer">
             <h1> {item.nombre_producto} </h1>
             <p>precio:</p>
             <p className="price">${item.precio}</p>
           </div>
-          <div className="buttom">
-            <button className="btn" onClick={() => addCarrito(item._id)}>
-              Añadir al carrito
+          <div className="btn col-auto md-12">
+            <button className="btn-warning" onClick={()=>{navigate('/home')}}>modificar</button>
+            <button className="btn-danger" onDoubleClick={()=>{navigate('/home')}}>borrar</button>
+            <button
+              className="btn-warning"
+              onClick={() => {
+                navigate("/home");
+              }}
+            >
+              modificar
             </button>
-            <div>
-              <Link to="#" className="a">
-                Vista
-              </Link>
-            </div>
+            <button
+              className="btn-danger"
+              onDoubleClick={() => {
+                navigate("/home");
+              }}
+            >
+              borrar
+            </button>
           </div>
         </div>
+        ):(null)
       );
     })
+
   ) : (
     <div class="loader">Loading...</div>
   );
