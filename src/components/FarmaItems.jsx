351e23ef509a021e8c1e0d12f675dfca18194a0d
juanosorioh <juangabrielosorioheredia@gmail.com>
2021-12-12 23:55:32 -0300

    boton de borrado funcionando

--- src/components/FarmaItems.jsx
+++ src/components/FarmaItems.jsx
@@ -4,17 +4,47 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import "../styles/Spin.css";
 import "animate.css";
+import { axios } from "axios";
+import { useNavigate } from 'react-router-dom';
 
 export const FarmaItems = () => {
   const value = useContext(DataContext);
   const addCarrito = value.addCarrito;
   const fetchMedicamentos = value.fetchMedicamentos;
   const [items] = value.items;
+  const navigate = useNavigate();
+////////////////////////////////7
+// Ejemplo implementando el metodo POST:
+async function postData(url = 'https://backend-farmacias-ya.herokuapp.com/producto/eliminar/', data) {
+  // Opciones por defecto estan marcadas con un *
+  const response = await fetch(url, {
+    method: 'PUT', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+////////////////////////////////7
+
+
+////////////////////////////////
+/* curl --location --request PUT 'https://backend-farmacias-ya.herokuapp.com/producto/eliminar/61b41221bc4fff45c7c8d594' */
+////////////////////////////////
+
+
 
   useEffect(() => {
     fetchMedicamentos();
   }, []);
-  console.log(value.items._id);
+  console.log(items._id);
   return items.length > 0 ? (
     items.map((item) => {
       return (
@@ -30,8 +60,8 @@ export const FarmaItems = () => {
             <p className="price">${item.precio}</p>
           </div>
           <div className="buttom">
-            <button className="btn" onClick={() => addCarrito(item._id)}>
-              Añadir al carrito
+            <button className="btn" onDoubleClick={() =>{postData("https://backend-farmacias-ya.herokuapp.com/producto/eliminar/"+item._id)}}>
+              Borrar
             </button>
             <div>
               <Link to="#" className="a">
---
+++ src/pages/FormularioModificar.jsx
@@ -0,0 +1,220 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import * as yup from "yup";
+import { useNavigate } from 'react-router-dom';
+import logo from "../assets/logo2.png";
+
+export const FormularioProductos = () => {
+  let schemaProductos = yup.object().shape({
+    nomFarmacia: yup.string().required(),
+    nomProducto: yup.string().required(),
+    nomProductoGen: yup.string().required(),
+    codigoProducto: yup.number().required().positive(),
+    imagen: yup.string().required(),
+    descrip: yup.string().required(),
+    precio: yup.number().required().positive(),
+    descuento: yup.number().required().positive(),
+    stock: yup.number().required().positive().integer()
+  });
+
+  const navigate = useNavigate();
+  const [nomFarmacia, setNomFarmacia] = useState("");
+  const [nomProducto, setNomProducto] = useState("");
+  const [nomProductoGen, setNomProductoGen]=useState("");
+  const [codigoProducto, setCodigoProducto]=useState("");
+  const [imagen, setImagen] = useState("");
+  const [descrip, setDescrip] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [descuento, setDescuento] = useState("");
+  const [stock, setStock] = useState("");
+  const [habilitado, setHabilitado] = useState(false);
+  //////////////////////////////
+
+  useEffect(() => {
+    schemaProductos
+      .isValid({
+        nomFarmacia,
+        nomProducto,
+        nomProductoGen,
+        codigoProducto,
+        imagen,
+        descrip,
+        precio,
+        descuento,
+        stock
+      })
+      .then((valid) => {
+        valid ? setHabilitado(true) : setHabilitado(false);
+      });
+  }, [nomFarmacia, nomProducto, nomProductoGen, codigoProducto, imagen, descrip, precio, descuento, stock]);
+
+  //////////////////////////////
+
+  const enviarD = async () => {
+    ////////////////////
+    let myHeaders = new Headers();
+
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      farmacia: nomFarmacia,
+      nombre_producto: nomProducto,
+      nombre_generico:nomProductoGen,
+    codigo_producto:codigoProducto,
+      img: imagen,
+      descripcion: descrip,
+      precio: precio,
+      descuento: descuento,
+      stock: stock
+    });
+
+    const options = {
+      method: "POST",
+      //mode:'no-cors',
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
+
+    const postData = await fetch(
+      "https://backend-farmacias-ya.herokuapp.com/productos/subir-producto",
+      options
+    );
+    setTimeout(function(){  navigate("/farmacia");; }, 4000);
+    //const res = postData.json();
+    //console.log(res);
+    ////////////////////
+  };
+
+
+
+  return (
+    <div>
+      <main className="form-signin mx-auto w-25 my-5">
+        <form>
+          <div className="row mx-auto w-25 my-5">
+          <img className="mb-4" src={logo} alt="" width="25" height="45" />
+          </div>
+          <div className="form-row mx-auto col-md-12">
+          <h3>Formulario de productos</h3>
+            <div className="form-group col-md-12">
+              <label for="inputEmail4">Nombre de la farmacia</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nombreF"
+                placeholder="Farmacia"
+                onChange={(e) => {
+                  setNomFarmacia(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+              <label for="inputPassword4">nombre del producto</label>
+              <input
+                type="text"
+                className="form-control"
+                id="producto"
+                placeholder="Producto"
+                onChange={(e) => {
+                  setNomProducto(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+              <label for="inputEmail4">Nombre generico</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nombreGen"
+                placeholder="nombre generico"
+                onChange={(e) => {
+                  setNomProductoGen(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+              <label for="inputEmail4">codigo de producto</label>
+              <input
+                type="number"
+                className="form-control"
+                id="codProd"
+                placeholder="codigo de producto"
+                onChange={(e) => {
+                  setCodigoProducto(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+            <label for="inputAddress">link de imagen</label>
+            <input
+              type="text"
+              className="form-control"
+              id="imagen"
+              placeholder="ingrese el link de la imagen"
+              onChange={(e) => {
+                setImagen(e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-group col-md-12">
+            <label for="inputAddress2">descripcion</label>
+            <input
+              type="text"
+              className="form-control"
+              id="descrip"
+              placeholder="detalles"
+              onChange={(e) => {
+                setDescrip(e.target.value);
+              }}
+            />
+          </div>
+            <div className="form-group col-md-12">
+              <label for="inputCity">Precio</label>
+              <input
+                type="number"
+                className="form-control"
+                id="precio"
+                onChange={(e) => {
+                  setPrecio(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+              <label for="inputZip">Descuento</label>
+              <input
+                type="number"
+                className="form-control"
+                id="decuento"
+                onChange={(e) => {
+                  setDescuento(e.target.value);
+                }}
+              />
+            </div>
+            <div className="form-group col-md-12">
+              <label for="inputZip">Stock</label>
+              <input
+                type="number"
+                className="form-control"
+                id="stock"
+                onChange={(e) => {
+                  setStock(e.target.value);
+                }}
+              />
+            </div>
+            <button
+            type="submit"
+            className="btn btn-warning col-md-12"
+            disabled={habilitado ? false : true}
+            onClick={(e) => {
+              enviarD(e.preventDefault());
+            }}
+          >
+            {habilitado ?"Enviar":"Deshabilitado"}
+          </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+};
